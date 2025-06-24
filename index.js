cc2ff01fd6cfd53f538cbc3bbdd5f7a5908f8737
Ruslan <4irus86@gmail.com>
2025-06-24 14:31:28 +0300

    create uploads folder

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import { handleValidationErrors, checkAuth } from './utils/index.js'
 import dotenv from 'dotenv';
 import multer from 'multer'
 import cors from 'cors'
+import fs from 'fs'
 
 
 mongoose
@@ -17,6 +18,9 @@ const app = express();
 
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
+    if (!fs.existsSync('uploads')) {
+      { fs.mkdirSync('uploads') }
+    }
     cb(null, 'uploads');
   },
   filename: (_, file, cb) => {
